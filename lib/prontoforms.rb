3d08c87a76dc7a70381b905338133ef78ab3ba13
Paul Holden <paul@codelunker.com>
2020-09-15 10:00:44 -0700

    feat(error): Errors are initialized with an error message

--- lib/prontoforms.rb
+++ lib/prontoforms.rb
@@ -2,5 +2,11 @@ require 'prontoforms/version'
 require 'prontoforms/client'
 
 module ProntoForms
-  class Error < StandardError; end
+  class Error < StandardError
+    attr_reader :message
+
+    def initialize(message)
+      @message = message
+    end
+  end
 end
