b1c2533082b55c7d1272ff5cbf22ae216a8b9ef1
Paul Holden <pholden@stria.com>
2020-12-07 20:50:46 -0800

    feat: add FormIteration

---
+++ lib/prontoforms/form_iteration.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'prontoforms/resource'
+
+module ProntoForms
+  # A form iteration is a form configuration, distinct from a form version
+  # in that version numbers increment when a form iteration is deployed.
+  class FormIteration < Resource
+    def self.resource_name
+      'iterations'
+    end
+
+    # @return [String] The form iteration identifier
+    property :id, key: 'identifier'
+    # @return [Integer] Version number
+    property :version, key: 'version'
+    # @return [String] Form iteration state
+    property :state, key: 'state'
+    # @return [String] Initiation method for the form iteration
+    property :initiation_method, key: 'initiationMethod'
+    # @return [String] Can dispatched forms of this iteration be declined
+    property :dispatched_declinable, key: 'dispatchDeclinable'
+
+    alias can_decline_dispatch? dispatched_declinable
+  end
+end
