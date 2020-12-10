5b6a9ccec920d6d86cf3f00ee1106b5a97ca1aa0
Paul Holden <pholden@stria.com>
2020-12-09 17:09:17 -0800

    ci: add sanity tests for paged/unpaged mock form data

--- spec/prontoforms/form_spec.rb
+++ spec/prontoforms/form_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe ProntoForms::Form do
   it 'can be retrieved in pages' do
     expect(forms.items).to all(be_a(ProntoForms::Form))
   end
+
+  describe 'mock data' do
+    context 'when paged' do
+      it 'excludes additional data' do
+        expect(forms.items.first.data.dig('activeVersion')).to be_nil
+      end
+    end
+
+    context 'when individual' do
+      it 'includes additional data' do
+        expect(form_space.form(1).data.dig('activeVersion')).not_to be_nil
+      end
+    end
+  end
 end
