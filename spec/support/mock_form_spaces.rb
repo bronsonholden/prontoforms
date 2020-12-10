66b16268d36bd2924cd7a9b51d5a0cc8de98016a
Paul Holden <pholden@stria.com>
2020-12-09 16:13:37 -0800

    ci(mock): add #mock_paged_data helper

--- spec/support/mock_form_spaces.rb
+++ spec/support/mock_form_spaces.rb
@@ -7,16 +7,7 @@ module MockFormSpaces
 
     app.namespace '/formspaces' do
       get do
-        num = 1000
-        s = params.fetch('s', '100').to_i
-        p = params.fetch('p', '0').to_i
-        size = [num - (p * s), 0].max
-        json_response 200, {
-          'totalNumberOfResults' => num,
-          'totalNumberOfPages' => (num / s) + 1,
-          'zone' => nil,
-          'pageData' => Array.new(size) { |i| mock_form_space(p * s + i + 1) }
-        }
+        json_response 200, mock_paged_data { |i| mock_form_space(i) }
       end
 
       get '/:id' do
--- spec/support/mock_prontoforms.rb
+++ spec/support/mock_prontoforms.rb
@@ -19,6 +19,21 @@ class MockProntoForms < Sinatra::Base
     data.to_json
   end
 
+  # Return paged mock data.
+  # @param num [Integer] Total number of items
+  # @param p [Integer] Page number to return
+  # @param s [Integer] Page size
+  def mock_paged_data(num = 1000, p = params.fetch('p', '1').to_i,
+                            s = params.fetch('s', '100').to_i, &block)
+    count = [num - (p * s), 0].max
+    {
+      'totalNumberOfResults' => num,
+      'totalNumberOfPages' => (num / s) + 1,
+      'zone' => nil,
+      'pageData' => Array.new(count) { |i| block.call(p * s + i + 1) }
+    }
+  end
+
   def mock_form_space(id = 1)
     {
       'identifier' => id.to_s,
