e5cd0dd0fd0bb5396c0c460f43b3bb41c1a70223
Paul Holden <pholden@stria.com>
2020-12-09 17:08:34 -0800

    ci: use some test vars for readability

--- spec/prontoforms/form_spec.rb
+++ spec/prontoforms/form_spec.rb
@@ -2,12 +2,14 @@
 
 RSpec.describe ProntoForms::Form do
   let(:client) { ProntoForms::Client.new('key', 'secret') }
+  let(:form_space) { client.form_space(1) }
+  let(:forms) { form_space.forms }
 
   it 'can be retrieved by ID' do
-    expect(client.form_space(1).form(1)).to be_a(ProntoForms::Form)
+    expect(form_space.form(1)).to be_a(ProntoForms::Form)
   end
 
   it 'can be retrieved in pages' do
-    expect(client.form_space(1).items).to all(be_a(ProntoForms::Form))
+    expect(forms.items).to all(be_a(ProntoForms::Form))
   end
 end
