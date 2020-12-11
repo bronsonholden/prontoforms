5ec2ab3c636e146217bd86ad2d7969e2585789ce
Paul Holden <pholden@stria.com>
2020-12-11 14:20:31 -0800

    ci: add test for Form#form_space_id
    Covers error fixed in 58ce907

--- spec/prontoforms/form_spec.rb
+++ spec/prontoforms/form_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe ProntoForms::Form do
   let(:client) { ProntoForms::Client.new('key', 'secret') }
   let(:form_space) { client.form_space(1) }
   let(:forms) { form_space.forms }
+  let(:form) { forms.items.first }
 
   it 'can be retrieved by ID' do
     expect(form_space.form(1)).to be_a(ProntoForms::Form)
@@ -11,6 +12,10 @@ RSpec.describe ProntoForms::Form do
 
   it 'can be retrieved in pages' do
     expect(forms.items).to all(be_a(ProntoForms::Form))
+  end
+
+  it 'has a form space' do
+    expect(form.form_space_id).to eq(form_space.id)
   end
 
   describe 'mock data' do
